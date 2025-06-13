a68be0f01c687a07cba326c2cf815b9f76b047dc
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2025-06-12 22:22:47 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/roles', [RoleController::class, 'index']);
+Route::resource('roles', RoleController::class);
